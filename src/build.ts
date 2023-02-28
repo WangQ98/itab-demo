a939251f4a65b51262106ce1c496e0cbc8e4dd0f
徐堃 <821289472@qq.com>
2023-02-28 09:42:12 +0800

    feat:添加入口文件，打包为lib

--- src/build.ts
+++ src/build.ts
@@ -1,5 +1,13 @@
+//组件
+import "@/styles/index.less";
+import "@unocss/reset/tailwind.css";
+import "gridstack/dist/gridstack.min.css";
+import "uno.css";
+import "./unocss-icon";
 import ITab from "@/layout/index.vue";
+//组件接口
+export { default as itabMaterials } from "@/utils/itabMaterials";
+//自带组件资源
+export { default as itabPackages } from "@/packages";
 
-export default {
-  ITab,
-};
+export default ITab;
